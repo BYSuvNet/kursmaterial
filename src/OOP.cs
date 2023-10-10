5e2ef97a2a591d7112346b6619e7d74aaccef906
Gus <gusgit@fastmail.org>
2023-10-10 19:42:41 +0200

    Update OOP

--- src/OOP.cs
+++ src/OOP.cs
@@ -2,10 +2,10 @@
 /*
     OBJEKTORIENTERAD PROGRAMMERING
 
-    INKAPSLING: * Handlar om att gruppera data (attribut) och metoder som opererar på datan i en enhet, 
-                ofta kallad en klass.
+    INKAPSLING: * Handlar om att gruppera data (attribut) och metoder som opererar på datan
+                  i en enhet kallad en klass.
                 * Kontrollerar åtkomst till objektens inre tillstånd genom publika, privata och 
-                skyddade åtkomstmodifierare (public, private, protected).
+                  skyddade åtkomstmodifierare (public, private, protected).
                 * Döljer komplexitet och detaljer genom att endast exponera det som är nödvändigt 
 
     ARV: * Gör det möjligt för en klass att ärva egenskaper och metoder från en annan klass.
@@ -13,12 +13,12 @@
          * En klass som en annan klass ärver från kallas för basklass, superklass, förälderklass.
     
     POLYMORFISM: * Ett koncept där objekt kan hanteras som instanser av deras basklass 
-                 eller subklass snarare än deras faktiska datatyp.
+                   eller subklass snarare än deras faktiska datatyp.
                  * En metod kan ha flera implementeringar beroende på objektet det kallas på.
 
     ABSTRAKTION: * Abstraktion är en av de viktigaste principerna i OOP.
                  * Handlar om att representera komplexa verkliga föremål eller system 
-                 som programmeringsmässiga entiteter (klasser).
+                   som programmeringsmässiga entiteter (klasser).
                  * Dölj onödiga detaljer och fokuserar på högnivåfunktionalitet.
                  * Hjälper till att bryta ner komplexa system i mindre, mer hanterbara delar.
                  * -> Inkapsling, arv och polyorfism möjliggör Abstraktion.
